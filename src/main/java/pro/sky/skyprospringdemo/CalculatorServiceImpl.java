a9b345e2beec78744c62a8bc4b1261c81bf883b4
Dany.Gen <Gentiev@vk.com>
2024-04-25 00:42:31 +0300

    HWGentiev2.4

--- src/main/java/pro/sky/skyprospringdemo/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/skyprospringdemo/CalculatorServiceImpl.java
@@ -7,15 +7,12 @@ public class CalculatorServiceImpl {
     public String calculator() {
         return "<b>Добро пожаловать в калькулятор</b>";
     }
-
     public String plusCalculator(int num1, int num2) {
         return num1 + " + " + num2 + " = " + (num1+num2);
     }
-
     public String minusCalculator(int num1, int num2) {
         return num1 + " - " + num2 + " = " + (num1-num2);
     }
-
     public String multiplyCalculator(int num1, int num2) {
         return num1 + " * " + num2 + " = " + (num1*num2);
     }
--- src/main/java/pro/sky/skyprospringdemo/SkyproSpringDemoApplication.java
+++ src/main/java/pro/sky/skyprospringdemo/SkyproSpringDemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SkyproSpringDemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SkyproSpringDemoApplication.class, args);
 	}
-
 }
